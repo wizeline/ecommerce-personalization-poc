a20e1d9eeb9248fb01495fa76c57b072139f2511
Joan Ginard <joan.ginard@wizeline.com>
2024-01-15 10:40:46 +0100

    first try to use langchaing in the previous codebase

--- generate-recommendations.js
+++ generate-recommendations.js
@@ -1,4 +1,6 @@
-import OpenAIAPI from "openai";
+import { LLMChain } from 'langchain/chains';
+import { OpenAI } from 'langchain/llms/openai';
+
 import { generatePrompt } from "./generate-prompt.js";
 import {
   readProductInfo,
@@ -8,7 +10,7 @@ import {
 import dotenv from "dotenv";
 dotenv.config();
 
-const openai = new OpenAIAPI({
+const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 
@@ -27,16 +29,12 @@ async function sendAPIPrompt() {
     });
     const prompt = generatePrompt(promptData);
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
-      messages: [
-        {
-          role: "user",
-          content: prompt,
-        },
-      ],
-    });
-    console.log(response.choices[0].message.content);
+    model = new OpenAI({ modelName: "gpt-3.5-turbo-1106", temperature: 0 })
+    const template =
+      'Be very technical when responding. Respond what you know about company: {company}'
+    const chain = new LLMChain({ llm: model, prompt })
+    const result = await chain.call({ company: companyName })
+    console.log(result.text)
   } catch (error) {
     console.error("Error al enviar la solicitud:", error);
   }
