6ecf56f51775f2d1089a28e763d66a84f2a9d9cf
Joan Ginard <joan.ginard@wizeline.com>
2024-01-19 15:02:19 +0100

    first example of estructured output through openapi functions

--- map-products-to-types-of-users.js
+++ map-products-to-types-of-users.js
@@ -1,35 +1,24 @@
 import { LLMChain, StuffDocumentsChain, MapReduceDocumentsChain } from "langchain/chains";
 import { PromptTemplate } from "@langchain/core/prompts";
-import { OpenAI } from "@langchain/openai";
-import { readProductInfo, } from "./read-database-info.js";
-import fs from "fs";
-const TYPES_OF_USERS = fs.readFileSync("./types_of_users.txt");
-
-
+import { ChatOpenAI } from "@langchain/openai";
+import { readCategories, readProductInfo, } from "./read-database-info.js";
+import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 export const mapProductsToTypesOfUsers = async () => {
   try {
-    const llm = new OpenAI({
-      modelName: "gpt-3.5-turbo",
-      temperature: 0,
-      // fail at the first rate limit error
-      maxRetries: 0,
-      // no more than two queries at the same time
-      maxConcurrency: 2,
-    });
+    const TYPES_OF_USERS = await readCategories()
+      .then((categories) => categories.map(({id, description}) => `* ${id}. ${description}\n\n`));
+
 
     const categorizeProductPrompt = PromptTemplate.fromTemplate(`
     you will help to decide to witch users a product will like, based on the product description.
     We will do the following:
     1. I'll give you the description and id of a product.
     2. I'll give you the list of types of users, each with a uniq id.
-    3. You will answer an SQL sentence for a table named "info_categories" that contains the related categories like this one :
-      
-      UPDATE info_categories set 
-    
+    3. You will answer the list of category ids that better match
 
     ====================
 
@@ -48,16 +37,50 @@ export const mapProductsToTypesOfUsers = async () => {
     --------------------
     ${TYPES_OF_USERS}
     --------------------
-
-    SQL_SENTENCE:
-
     `);
 
-    const chain = categorizeProductPrompt.pipe(llm);
+    const llm = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      temperature: 0,
+      // fail at the first rate limit error
+      maxRetries: 0,
+      // no more than two queries at the same time
+      maxConcurrency: 2,
+    })
+    .bind({
+      functions: [{
+        name: "extractor",
+        description: "Extracts recommended ids from the input",
+        parameters: {
+          type: "object",
+          properties: {
+            recomended_category_ids: {
+              type: "array",
+              description: "the recommended category ids based on the description of the product",
+              items: {
+                type: Number,
+                description: 'id of a category'
+              }
+            },
+            why: {
+              type: "string",
+              description: "the reasons behing why you recommended this specific categories",
+            },
+          },
+          required: ["recomended_category_ids", "why"],
+        },
+      }],
+      function_call: { name: "extractor" },
+    })
 
+
+
+    const chain = categorizeProductPrompt
+      .pipe(llm)
+      .pipe(new JsonOutputFunctionsParser());
 
     return readProductInfo()
-      .then((productsInfo) => chain.batch(productsInfo.slice(1, 3)));
+      .then((productsInfo) => chain.invoke(productsInfo[1]) );
 
   } catch (error) {
     console.error("Error al ejecutar la cadena:", error);
--- map-products-to-types-of-users.test.js
+++ map-products-to-types-of-users.test.js
@@ -5,11 +5,11 @@ import { Sequelize, Model, DataTypes } from 'sequelize';
 
 describe("map-products-to-types-of-users", () => {
 
-  it("runs properly", () => {
+  it.only("runs properly", () => {
     return mapProductsToTypesOfUsers().then(console.log);
   }, 120000)
 
-  it.only("can update database easily", async () => {
+  it("can update database easily", async () => {
 
 
     const sequelize = new Sequelize({
