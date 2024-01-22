0a193a1bae3be28feff3ca30b90f8f1b6e995976
Joan Ginard <joan.ginard@wizeline.com>
2024-01-22 10:48:27 +0100

    better code structure and json format

--- map-products-to-types-of-users.js
+++ map-products-to-types-of-users.js
@@ -1,7 +1,7 @@
 import { LLMChain, StuffDocumentsChain, MapReduceDocumentsChain } from "langchain/chains";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
-import { readCategories, readProductsWithoutCategory, setProductCategories} from "./read-database-info.js";
+import { readCategories, readProductsWithoutCategory, setProductCategories } from "./read-database-info.js";
 import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 import cliProgress from "cli-progress";
 
@@ -11,7 +11,7 @@ dotenv.config();
 export const mapProductsToTypesOfUsers = async () => {
   try {
     const TYPES_OF_USERS = await readCategories()
-      .then((categories) => categories.map(({id, description}) => `* ${id}. ${description}\n\n`));
+      .then((categories) => categories.map(({ id, description }) => `* ${id}. ${description}\n\n`));
 
 
     const categorizeProductPrompt = PromptTemplate.fromTemplate(`
@@ -46,51 +46,50 @@ export const mapProductsToTypesOfUsers = async () => {
       // fail at the first rate limit error
       maxRetries: 5,
       // no more than two queries at the same time
-    })
-    .bind({
-      functions: [{
-        name: "extractor",
-        description: "Extracts recommended ids from the input",
-        parameters: {
-          type: "object",
-          properties: {
-            category_ids: {
-              type: "array",
-              description: "the recommended category ids based on the description of the product",
-              items: {
-                type: Number,
-                description: 'id of a category'
-              }
-            },
-            why: {
-              type: "string",
-              description: "the reasons behing why you recommended this specific categories",
-            },
-          },
-          required: ["category_ids", "why"],
-        },
-      }],
-      function_call: { name: "extractor" },
-    })
+    });
 
 
 
     const chain = categorizeProductPrompt
-      .pipe(llm)
+      .pipe(llm.bind({
+        functions: [{
+          name: "extractor",
+          description: "Extracts recommended ids from the input",
+          parameters: {
+            type: "object",
+            properties: {
+              category_ids: {
+                type: "array",
+                description: "the recommended category ids based on the description of the product",
+                items: {
+                  type: Number,
+                  description: 'id of a category'
+                }
+              },
+              why: {
+                type: "string",
+                description: "the reasons behing why you recommended this specific categories",
+              },
+            },
+            required: ["category_ids", "why"],
+          },
+        }],
+        function_call: { name: "extractor" },
+      }))
       .pipe(new JsonOutputFunctionsParser());
 
-   const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
     return readProductsWithoutCategory()
       .then(async (infos) => {
 
         bar.start(infos.length, 0);
 
-        for(const info of infos){
+        for (const info of infos) {
           await chain.invoke(info)
-          .then(({category_ids, why}) => {
-            return setProductCategories(info, category_ids, why)
-          })
+            .then(({ category_ids, why }) => {
+              return setProductCategories(info, category_ids, why)
+            })
 
           bar.increment();
         }
