33ba4841258950c6d9650f45dd7b4731b0ddaaf1
Joan Ginard <joan.ginard@wizeline.com>
2024-01-30 12:35:17 +0100

    adding netlify function

--- src/components/serverButton.tsx
+++ src/components/serverButton.tsx
@@ -1,10 +1,15 @@
 import type React from "react";
 import { useState } from "react";
-const fromTheServer: () => string = () => `Random number from the server ${Math.random()}`;
+import fromTheServer  from "../../netlify/functions/from-the-server.mts";
 
+
+const fetchFromTheServer: () => Promise<string> = () => fetch("./.netlify/functions/from-the-server").then(r => r.text());
 
 export const ServerButton: React.FC = () => {
     const [message, setMessage] = useState("initial message");
 
-    return <button onClick={() => setMessage(fromTheServer())}>{message}</button>;
+    return <>
+        <button onClick={() => fetchFromTheServer().then(setMessage)}>Call the server</button>
+        <p>{message}</p>
+    </>;
 }
